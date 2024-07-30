bd1e5a03e6627880accd148383877629e2653651
Talha Khalil <talhakhalil535@gmail.com>
2024-07-30 17:49:00 +0500

    add validations

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,3 +1,12 @@
 class Tree < ApplicationRecord
     belongs_to :user
-end
+  
+    validates :name, presence: true, length: { minimum: 5, message: 'must be at least 5 characters long' }
+  
+    validates :instagram, presence: true, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: 'Invalid URL format' }
+  
+    validates :twitter, presence: true, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: 'Invalid URL format' }
+  
+    validates :youtube, presence: true, format: { with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/, message: 'Invalid URL format' }
+  end
+  
